1f2619cfcd6f8ab44402fe7d0c19f10599d5e79a
financewiki-park <plisilla@naver.com>
2026-09-26 17:19:30 +0900

    Embed Kindle launcher icon metadata

--- ime-package/scriptlets/install-resume-launcher.sh
+++ ime-package/scriptlets/install-resume-launcher.sh
@@ -2,14 +2,14 @@
 set -eu
 STATE=/mnt/us/korean-ime
 SOURCE=./scriptlets/start-korean-ime.sh
-ICON_SOURCE='./scriptlets/Korean IME Start.png'
+ICON_SOURCE=./scriptlets/icon.png
 RESUME="$STATE/start-korean-ime.sh"
 DOCUMENT='/mnt/us/documents/Korean IME Start.sh'
-ICON_DOCUMENT='/mnt/us/documents/Korean IME Start.png'
+ICON="$STATE/icon.png"
 mkdir -p "$STATE" /mnt/us/documents
 cp "$SOURCE" "$RESUME"
 chmod 755 "$RESUME"
 cp "$SOURCE" "$DOCUMENT"
 chmod 755 "$DOCUMENT"
-cp "$ICON_SOURCE" "$ICON_DOCUMENT"
+cp "$ICON_SOURCE" "$ICON"
 echo "resume_launcher=$DOCUMENT"
--- ime-package/scriptlets/start-korean-ime.sh
+++ ime-package/scriptlets/start-korean-ime.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# Name: Korean IME Start
+# Author: financewiki-park
+# Icon: /mnt/us/korean-ime/icon.png
+# DontUseFBInk
 # korean-ime-resume-launcher
 exec /var/local/kmc/bin/kpm launch korean-ime
